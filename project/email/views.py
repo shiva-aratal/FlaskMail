8d6a0295ab1e3f82be385fcd50460573bdd986ba
shiva-aratal <shiva.aratal@gmail.com>
2018-01-03 21:48:54 -0800

    Email send out setup

--- project/email/views.py
+++ project/email/views.py
@@ -2,25 +2,40 @@ from flask import jsonify, request, Blueprint
 from flask import current_app as flask_app
 from flask_mail import Message
 from .. import mail
+import os
+import sendgrid
+from sendgrid.helpers.mail import *
+sg = sendgrid.SendGridAPIClient(apikey="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")
+
+
 
 
 email_blueprint = Blueprint('email', __name__)
 
 
+
+
+@email_blueprint.route('/email', methods=['POST'])
 def send_email():
 	content = request.get_json()
 
 	validContent = False
 	if 'to' in content and 'subject' in content and 'body' in content and 'from' in content:
 		validContent = True
+	if validContent:
+		from_email = Email(content['from'])
+		to_email = Email(content['to'])
+		subject = content['subject']
+		content_body = Content("text/plain",content['body'])
+		mail = Mail(from_email, subject, to_email, content_body)
 
-	response = sg.client.mail.send.post(request_body=mail.get())
+		response = sg.client.mail.send.post(request_body=mail.get())
 		return jsonify({'Message': 'Email Sent'})
 	else:
 		return jsonify({'Error': 'Missing Parameters'})
 
 
-
+@email_blueprint.route('/testapp', methods=['POST'])
 def send_test_email():
 	content = request.get_json()
 
@@ -31,4 +46,4 @@ def send_test_email():
 		return jsonify({'Message': 'Input Parameters Returned', 'Mail send to': content['to'], 'Mail Subject': content['subject'],
 			'Mail Body': content['body']})
 	else:
-		return jsonify({'Error': 'Missing Input'})
+		return jsonify({'Error': 'Invalid Input'})
